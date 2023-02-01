0e8cafb1c7e5a8f449135e9ba66772115a622a5c
Rahi999 <ahilh871@gmail.com>
2023-02-01 10:13:00 +0530

    initial commit

--- day-1/tests/test.js
+++ day-1/tests/test.js
@@ -1,6 +1,10 @@
 const {By,Key,Builder} = require("selenium-webdriver");
 require("chromedriver");
 
+const USERNAME = "ahilh871";
+const KEY = 'E3AzQrTMDiBPtYIZ7OhqNRSSqGjh0SpnTSFuF6oXoVk6LW2ki7';
+const GRID_HOSt
+
 const testFunction = async () => {
     const searchString = "Masai School Admission";
     let driver = await new Builder().forBrowser("chrome").build();
