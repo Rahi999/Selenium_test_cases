685b8f149f580aba2077998efed46ed4f8f015f5
Rahi999 <rahimansari4560@gmail.com>
2023-02-05 07:58:29 +0530

    defined boolean in python

--- Masai/Python/main.py
+++ Masai/Python/main.py
@@ -4,9 +4,10 @@ print("Hello, World!")
 # Defining Variables;
 name = "Rahim Ansari"
 age = 19
+is_Maried = False
 
 # Changing value of same variable's name
 name = "Ahil"
 age = 19.0
 
-print(name,age)
+print(name,age,is_Maried)
