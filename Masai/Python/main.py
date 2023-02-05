ad81476e44d227dd69e4e0b7c66688a3b88d8830
Rahi999 <rahimansari4560@gmail.com>
2023-02-05 07:57:10 +0530

    defined variables and changed its value

--- Masai/Python/main.py
+++ Masai/Python/main.py
@@ -1,3 +1,12 @@
 # Printing string(Hello world);
-
 print("Hello, World!")
+
+# Defining Variables;
+name = "Rahim Ansari"
+age = 19
+
+# Changing value of same variable's name
+name = "Ahil"
+age = 19.0
+
+print(name,age)
