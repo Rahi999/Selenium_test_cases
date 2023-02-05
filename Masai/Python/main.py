3735c6d05b00347a2a35bce01ab3d9534727523d
Rahi999 <rahimansari4560@gmail.com>
2023-02-05 08:00:45 +0530

    Task Completed

--- Masai/Python/main.py
+++ Masai/Python/main.py
@@ -11,3 +11,12 @@ name = "Ahil"
 age = 19.0
 
 print(name,age,is_Maried)
+
+# Task 
+# Add a person with name "Tony Stark", Tony's age is 51 years old, Tony is a genius
+
+name = str("Tony Stark")
+tony_age = 51
+is_genius = True
+
+print(name,tony_age,is_genius)
